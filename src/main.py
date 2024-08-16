68d46ca217a8e7b88a405c0431d36c6c432764ec
IRSHIT033 <mukherjeeirshit50@gmail.com>
2024-08-17 02:29:55 +0530

    feat: serverless lambda init

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from src.api.endpoints import router as api_endpoint_router
 from src.config.events import execute_backend_server_event_handler, terminate_backend_server_event_handler, execute_data_seed_event_handler
 from src.config.manager import settings
 
+from mangum import Mangum
 
 def initialize_backend_application() -> fastapi.FastAPI:
     app = fastapi.FastAPI(**settings.set_backend_app_attributes)  # type: ignore
@@ -33,11 +34,15 @@ def initialize_backend_application() -> fastapi.FastAPI:
     )
 
     app.include_router(router=api_endpoint_router, prefix=settings.API_PREFIX)
+    
+    
+
 
     return app
 
 
 backend_app: fastapi.FastAPI = initialize_backend_application()
+handler=Mangum(backend_app)
 
 if __name__ == "__main__":
     uvicorn.run(
